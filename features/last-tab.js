b0c4315cc0949d828c999b1c32817bca93c8fc03
TwelveStepSuite <23rd@vivaldi.net>
2015-07-02 00:44:06 +0400

    Click active tab to go to previous tab

--- features/last-tab.js
+++ features/last-tab.js
@@ -1,18 +1,20 @@
-var browser=document.body.querySelector('#browser');
-var list=[];
+var browser = document.body.querySelector('#browser');
+var list = [];
 function listTabs() {
-    var tabs=browser.querySelectorAll('#tabs>.tab');
-    list=[];
-    for(var i=0;i<tabs.length;i++){
+    //getting tabs
+    var tabs = browser.querySelectorAll('#tabs>.tab');
+    list = [];
+    for(var i = 0;i < tabs.length; i++){
         list.push(tabs[i]);
     }
 
+    //Sort tabs to get last and pre-last
     list.sort(function(a,b){
         return recent(b) - recent(a);
-        });
+    });
 }
 
-function recent(tab){
+function recent(tab) {
     var page = document.querySelector('.webpageview webview[tab_id="'+tab.dataset.tabId+'"]');
     if(page) {
         page = page.parentNode.parentNode.parentNode.parentNode;
@@ -34,9 +36,7 @@ browser.addEventListener('mousedown', function(e){
 
             listTabs();
             dispatchMouseEvent(list[1], 'mousedown', true, true);
-            var webview = document.querySelector('#webview-container webview[tab_id="'+active.dataset.tabId+'"]');
-            webview.executeScript({ code: "document.body.scrollTop=0" });
-            return;
+            break;
         }
     }
 });
